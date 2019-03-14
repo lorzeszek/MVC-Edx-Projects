37b69c025a873f64c7e38c673809e24e7d412087
lorzeszek <lucas.orzeszek@gmail.com>
2019-03-14 17:59:15 +0100

    commit 14.03.2019

---
+++ Lambda/Models/Person.cs
@@ -0,0 +1,9 @@
+namespace Lambda.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
